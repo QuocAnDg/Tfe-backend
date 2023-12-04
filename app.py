2277e30918a4e6f35caf0f3442beab8d50351241
QuocAnDg <quocand40@gmail.com>
2023-12-04 18:07:00 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app, origins="http://localhost:5173")
+
+# Neo4j configuration
+uri = os.getenv('DB_URI')
+username = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+
+driver = GraphDatabase.driver(uri, auth=(username, password))
+
+@app.route('/nodes')
+def tous_les_noeuds():
+    query = "MATCH (n:User) RETURN n"
+    with driver.session() as session:
+        nodes = session.run(query).data()
+    return jsonify(nodes)
+
+if __name__ == '__main__':
+    app.run(debug=True)
