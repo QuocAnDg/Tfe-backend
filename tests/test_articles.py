74ade7e3749767f1b9e13660468f0feab3ed30a1
QuocAnDg <quocand40@gmail.com>
2023-12-11 21:35:49 +0100

    docs: remove comments in tests

--- tests/test_articles.py
+++ tests/test_articles.py
@@ -9,12 +9,10 @@ def client():
 
 
 def test_add_article(client):
-    # Define a sample article data
     article_data = {"nom": "Sample Article"}
 
-    # Make a POST request to the add_article endpoint
     response = client.post('/articles', json=article_data)
-    # Check if the response status code is 200 (OK)
+
     assert response.status_code == 308
 
 
