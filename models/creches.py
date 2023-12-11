04916e2ea84a968a3f6d759ccc14c8dd734acd09
tqueguin <tanguyqueguineur@gmail.com>
2023-12-11 15:05:08 +0100

    add article list to creches route

--- models/creches.py
+++ models/creches.py
@@ -2,6 +2,8 @@ from utils import DBservice
 
 
 def read_une_creche(nom):
-    query = f"""MATCH (creche:Creche {{nom : "{nom}"}})
-        RETURN creche;"""
+    query = f"""MATCH (creche:Creche {{nom : "{nom}"}})-[c:CONTIENT]->(article:Article)
+        WITH creche, COLLECT({{article: article, quantite: c.quantite}}) AS articleList
+        RETURN creche, articleList;"""
+
     return DBservice.runquery(query)
