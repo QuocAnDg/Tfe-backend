acc805d38be75b9386e9720376e41ccc78481ead
tqueguin <tanguyqueguineur@gmail.com>
2023-12-12 07:40:09 +0100

    change tournees route

--- models/tournees.py
+++ models/tournees.py
@@ -7,15 +7,14 @@ def read_toutes_les_tournees():
     return DBservice.runquery(query)
 
 
-def read_une_tournee(tourneeid):
-    query = f"""MATCH (tournee:Tournee)-[:LIVRE]->(creche:Creche)-[c:CONTIENT]->(article:Article)
-        WHERE tournee.id = {tourneeid}
+def read_une_tournee(nom):
+    query = f"""MATCH (tournee:Tournee{{nom:"{nom}"}})-[:LIVRE]->(creche:Creche)-[c:CONTIENT]->(article:Article)
         WITH creche, COLLECT({{article: article, quantite: c.quantite}}) AS articleList
         RETURN creche,articleList;"""
     return DBservice.runquery(query)
 
 
-def read_tournee_from_nom(nom):
+def read_tournee(nom):
     query = f"""MATCH (tournee:Tournee {{nom: "{nom}"}})
         RETURN tournee;"""
     return DBservice.runquery(query)
@@ -23,6 +22,12 @@ def read_tournee_from_nom(nom):
 
 def creer_tournee(nom):
     query = f"""CREATE (t:Tournee{{nom:"{nom}"}});"""
+    return DBservice.runquery(query)
+
+
+def delete_tournee(nom):
+    # TODO: supprimer les relations de la node, les creches et les relations entre creche et article
+    query = f"""MATCH (tournee:Tournee{{nom:"{nom}"}}) DELETE TOURNEE;"""
     return DBservice.runquery(query)
 
 
--- routes/tournees.py
+++ routes/tournees.py
@@ -10,9 +10,9 @@ def get_tournees():
     return jsonify(tournees.read_toutes_les_tournees())
 
 
-@bp_tournees.route('/<tournee_id>', methods=['GET'])
-def get_tournee(tournee_id):
-    return jsonify(tournees.read_une_tournee(tournee_id))
+@bp_tournees.route('/<nom>', methods=['GET'])
+def get_tournee(nom):
+    return jsonify(tournees.read_une_tournee(nom))
 
 
 @bp_tournees.route('/', methods=['POST'])
@@ -20,7 +20,7 @@ def add_tournee():
     nom_tournee = request.json.get("nom", None)
     liste_creches = request.json.get("crèches", None)
 
-    tournee_found = tournees.read_tournee_from_nom(nom_tournee)
+    tournee_found = tournees.read_tournee(nom_tournee)
 
     if len(tournee_found) != 0:
         return jsonify({"msg": "Erreur, il existe déjà une tournée avec ce nom"}), 401
@@ -31,3 +31,13 @@ def add_tournee():
         tournees.ajouter_creche_a_tournee(nom_tournee, creche["nom"])
 
     return jsonify("ok")
+
+
+@bp_tournees.route('/<nom>', methods=['DELETE'])
+def delete_tournee(nom):
+    tournee_found = tournees.read_tournee(nom)
+
+    if len(tournee_found) == 0:
+        return jsonify({"msg": "Erreur, il n'existe pas de tournée avec ce nom"}), 401
+
+    return jsonify(tournees.delete_tournee(nom))
