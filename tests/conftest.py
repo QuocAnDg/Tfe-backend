ba5acafa5de15e314ea86321d74fdcb95654c4f7
QuocAnDg <quocand40@gmail.com>
2023-12-13 16:58:48 +0100

    fix: modify fixture annotation on mock functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,21 +10,19 @@ def client():
     with app.test_client() as client:
         yield client
 
-
-
-@pytest.fixture(scope="module")
+@pytest.fixture
 def valid_auth_token(client):
     with app.app_context():
         client.application.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY")
         access_token = create_access_token(identity="test_identity")
         return access_token                                     
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def mock_client_post(client):
     with patch.object(client, 'post', return_value=Mock(status_code=200, json_data={"status": "success"})):
         yield client.post 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def mock_client_delete(client):
     with patch.object(client, 'delete', return_value=Mock(status_code=200, json_data={"status": "success"})):
         yield client.delete 
